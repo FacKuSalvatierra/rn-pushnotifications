f0c8544a69cefa46e95d5dcf75c6ff236103143a
Facundo <facku995@gmail.com>
2024-09-29 00:40:21 -0300

    first commit

---
+++ data/taskData.ts
@@ -0,0 +1,62 @@
+export const taskData = [
+    {
+        id: 1,
+        title: 'Task 1',
+        description: 'This is task 1',
+        status: 'pending'
+    },
+    {
+        id: 2,
+        title: 'Task 2',
+        description: 'This is task 2',
+        status: 'completed'
+    },
+    {
+        id: 3,
+        title: 'Task 3',
+        description: 'This is task 3',
+        status: 'pending'
+    },
+    {
+        id: 4,
+        title: 'Task 4',
+        description: 'This is task 4',
+        status: 'completed'
+    },
+    {
+        id: 5,
+        title: 'Task 5',
+        description: 'This is task 5',
+        status: 'pending'
+    },
+    {
+        id: 6,
+        title: 'Task 6',
+        description: 'This is task 6',
+        status: 'completed'
+    },
+    {
+        id: 7,
+        title: 'Task 7',
+        description: 'This is task 7',
+        status: 'pending'
+    },
+    {
+        id: 8,
+        title: 'Task 8',
+        description: 'This is task 8',
+        status: 'completed'
+    },
+    {
+        id: 9,
+        title: 'Task 9',
+        description: 'This is task 9',
+        status: 'pending'
+    },
+    {
+        id: 10,
+        title: 'Task 10',
+        description: 'This is task 10',
+        status: 'completed'
+    }
+]
